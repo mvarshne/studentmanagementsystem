0c668ec0a777f478dc4ae799076ae1370ae8797d
mavarshn <mavarshn@ciena.com>
2017-12-10 11:26:55 +0530

    collect student details and show them

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -11,8 +11,35 @@ namespace StudentManagementSystem
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the student management system..");
-            
-            
+
+            Console.WriteLine("how many students do you want to add, please add at least one..thanks..?");
+
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            Student[] students = new Student[numberOfStudents];
+
+            int stIndex = 1;
+
+            while (stIndex <= numberOfStudents)
+            {
+                Console.WriteLine($"collecting details for student {stIndex}");
+                Console.WriteLine("Name?");
+                string name = Console.ReadLine();
+                Console.WriteLine("State?");
+                string state = Console.ReadLine();
+
+                students[stIndex - 1] = StudentsFactory.Instance.AddNewStudent(name, state);
+                stIndex++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("here are the details of students entered..");
+
+            foreach(var student in students)
+            {
+                Console.WriteLine(StudentUtil.GetStudentDetailsInStringFormat(student));
+            }
+
             Console.ReadLine();
         }
     }
