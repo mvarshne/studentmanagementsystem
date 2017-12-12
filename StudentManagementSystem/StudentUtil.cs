2ca9ccfa64547cb06afc7a877473ab88f68e50b9
mavarshn <mavarshn@ciena.com>
2017-12-12 16:24:19 +0530

    adding a method again 12 Dec 04:24 pm

--- StudentManagementSystem/StudentUtil.cs
+++ StudentManagementSystem/StudentUtil.cs
@@ -12,5 +12,24 @@ namespace StudentManagementSystem
         {
             return string.Format($"Student Id: {st.Id}, Name: {st.Name}, and State: {st.State}");
         }
+
+        public static Student[] RemoveStudent(Student[] students, int stid)
+        {
+            List<Student> studentList = new List<Student>(students);
+            int indexOfStudentToBeRemoved = -1;
+            foreach (var student in studentList)
+            {
+                if (student.Id == stid)
+                {
+                    indexOfStudentToBeRemoved = studentList.IndexOf(student);
+                    break;
+                }
+            }
+            if (indexOfStudentToBeRemoved != -1)
+            {
+                studentList.RemoveAt(indexOfStudentToBeRemoved);
+            }
+            return studentList.ToArray();
+        }
     }
 }
